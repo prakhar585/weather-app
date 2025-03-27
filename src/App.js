517a7c9a8325c660d2d45cf669e1d48c8b50a319
unknown <prakharchaturvedi9860@gmail.com>
2025-03-27 19:34:53 +0530

    First working Draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
+  const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const handleSubmit = (e) => {
+  
+    e.preventDefault();
+
+    const fetchCity = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.weatherapi.com/v1/current.json`,
+          {
+            params: {
+              key: API_KEY,
+              q: city,
+            },
+          }
+        );
+
+        const weatherofCity = response.data;
+        
+        const newObj = {
+          Temperature:weatherofCity.current.temp_c ,
+          Humidity: weatherofCity.current.humidity,
+          Condition: weatherofCity.current.condition.text ,
+          WindSpeed:weatherofCity.current.wind_kph ,
+        };
+       
+       
+        setWeatherData(newObj);
+       
+       
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCity();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        ></input>
+        <button type="submit">Search</button>
+      </form>
+      {weatherData &&
+      <div className="weather-cards">
+        <div className="weather-card">Temperature: {weatherData.Temperature}</div>
+        <div className="weather-card">Humidity: {weatherData.Humidity}</div>
+        <div className="weather-card">Condition: {weatherData.Condition}</div>
+        <div className="weather-card">Wind Speed: {weatherData.WindSpeed}</div>
+      </div>}
     </div>
   );
 }
