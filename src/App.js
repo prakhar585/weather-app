f750e46876445259836468e7305525fc8e88e1b2
unknown <prakharchaturvedi9860@gmail.com>
2025-03-28 16:11:07 +0530

    styled and ready to submit for first test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-const API_KEY = '83f1e17831b944118ab125945252703';
+const API_KEY = "83f1e17831b944118ab125945252703";
 function App() {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+  const [loadingData, setLoadingData] = useState(false);
 
   // const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
   const handleSubmit = (e) => {
-  
     e.preventDefault();
 
     const fetchCity = async () => {
+      setLoadingData(true);
       try {
         const response = await axios.get(
           `https://api.weatherapi.com/v1/current.json`,
@@ -25,20 +26,21 @@ function App() {
         );
 
         const weatherofCity = response.data;
-        
+
         const newObj = {
-          Temperature:weatherofCity.current.temp_c ,
+          Temperature: weatherofCity.current.temp_c,
           Humidity: weatherofCity.current.humidity,
-          Condition: weatherofCity.current.condition.text ,
-          WindSpeed:weatherofCity.current.wind_kph ,
+          Condition: weatherofCity.current.condition.text,
+          WindSpeed: weatherofCity.current.wind_kph,
         };
-       
-       
+
         setWeatherData(newObj);
-       
-       
       } catch (error) {
         console.error(error);
+        setWeatherData(null);
+        window.alert("Failed to fetch weather data");
+      } finally {
+        setLoadingData(false);
       }
     };
 
@@ -55,13 +57,39 @@ function App() {
         ></input>
         <button type="submit">Search</button>
       </form>
-      {weatherData &&
-      <div className="weather-cards">
-        <div className="weather-card">Temperature: {weatherData.Temperature}</div>
-        <div className="weather-card">Humidity: {weatherData.Humidity}</div>
-        <div className="weather-card">Condition: {weatherData.Condition}</div>
-        <div className="weather-card">Wind Speed: {weatherData.WindSpeed}</div>
-      </div>}
+      {loadingData ? (
+        <p>Loading data...</p>
+      ) : (
+        <>
+          {weatherData && (
+            <div className="weather-cards">
+              <div className="weather-card">
+                <div>
+                  <b>Temperature:</b>
+                  <br />
+                  {weatherData.Temperature} °C{" "}
+                </div>
+              </div>
+              <div className="weather-card">
+                <div>
+                <b>Humidity:</b> <br /> {weatherData.Humidity}%
+                </div>
+              </div>
+              <div className="weather-card">
+                <div><b>Condition:</b>
+                <br /> {weatherData.Condition}
+                </div>
+              </div>
+              <div className="weather-card">
+                <div>
+                <b>Wind Speed:</b>
+                <br /> {weatherData.WindSpeed} kph
+                </div>
+              </div>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
